fd67c6032fbfe51a6365e386b9a193ce8aaac4e2
MUHAMMAD SARIM <muhammad.sarim001100@gmail.com>
2025-02-28 15:56:42 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,138 @@
+
+// let todo = document.getElementById("todoValue");
+// let list = document.getElementById("list");
+
+// function addTodo() {
+//     if (todo.value.trim() === "") {
+//         alert("Please enter a task!");
+//         return;
+//     }
+//     list.innerHTML += `<li class="lists">
+//                 <input type="text class="main_input" value="${todo.value}" disabled>
+//                 <button onclick="deleteTodo(event)">Delete</button>
+//                 <button onclick="editTodo(event)">Edit</button>
+//             </li>`;
+
+//     todo.value = "";
+// }
+
+// function editTodo(event) {
+//     let target = event.target;
+//     let li = target.parentNode;
+//     let input = li.querySelector("input");
+//     let editButton = li.querySelector("button:nth-child(3)");
+
+//     if (editButton.innerText === "Edit") {
+//         input.disabled = false;
+//         editButton.innerText = "Update";
+//     } else {
+//         input.disabled = true;
+//         editButton.innerText = "Edit";
+//     }
+// }
+
+// function deleteTodo(event) {
+//     let li = event.target.parentNode;
+//     li.remove();
+
+
+
+let todo = document.getElementById("todoValue");
+let list = document.getElementById("list");
+
+
+document.addEventListener("DOMContentLoaded", loadTodos);
+
+function addTodo() {
+    if (todo.value.trim() === "") {
+        alert("Please enter a task!");
+        return;
+    }
+
+
+    const newTodo = {
+        text: todo.value,
+        completed: false,
+    };
+
+
+    addTodoToList(newTodo);
+
+
+    saveTodosToLocalStorage();
+
+
+    todo.value = "";
+}
+
+function addTodoToList(todoItem) {
+    list.innerHTML += `<li class="lists">
+                <input type="checkbox" class="todo-checkbox" onchange="toggleCompletion(event)" ${todoItem.completed ? "checked" : ""}>
+                <input type="text" class="main_input" value="${todoItem.text}" disabled>
+                <button class="del" onclick="deleteTodo(event)">Delete</button>
+                <button class="edit" onclick="editTodo(event)">Edit</button>
+            </li>`;
+
+
+    if (todoItem.completed) {
+        const lastLi = list.lastElementChild;
+        const input = lastLi.querySelector(".main_input");
+        input.classList.add("completed");
+    }
+}
+
+function editTodo(event) {
+    let target = event.target;
+    let li = target.parentNode;
+    let input = li.querySelector(".main_input");
+    let editButton = li.querySelector("button:nth-child(4)");
+
+    if (editButton.innerText === "Edit") {
+        input.disabled = false;
+        editButton.innerText = "Update";
+    } else {
+        input.disabled = true;
+        editButton.innerText = "Edit";
+        saveTodosToLocalStorage();
+    }}
+    function deleteTodo(event) {
+        let li = event.target.parentNode;
+        li.remove();
+        saveTodosToLocalStorage();
+    }
+
+    function toggleCompletion(event) {
+        let checkbox = event.target;
+        let li = checkbox.parentNode;
+        let input = li.querySelector(".main_input");
+
+        if (checkbox.checked) {
+            input.classList.add("completed");
+        } else {
+            input.classList.remove("completed");
+        }
+
+        saveTodosToLocalStorage(); 
+    }
+
+    function saveTodosToLocalStorage() {
+        const todos = [];
+        const todoItems = list.querySelectorAll(".lists");
+
+        todoItems.forEach((item) => {
+            const text = item.querySelector(".main_input").value;
+            const completed = item.querySelector(".todo-checkbox").checked;
+            todos.push({ text, completed });
+        });
+
+        localStorage.setItem("todos", JSON.stringify(todos));
+    }
+
+    function loadTodos() {
+        const todos = JSON.parse(localStorage.getItem("todos")) || [];
+
+        todos.forEach((todoItem) => {
+            addTodoToList(todoItem);
+        });
+
+    }
